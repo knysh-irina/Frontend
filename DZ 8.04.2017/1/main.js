69fd6ba7a69db806df0de250e4b13b0b991e5508
Oleynik Irina <oleyniktsirina@gmail.com>
2017-04-21 13:18:49 +0300

    DZ1, 2

---
+++ DZ 8.04.2017/1/main.js
@@ -0,0 +1,10 @@
+
+'use strict';
+
+
+    var person = prompt("Please enter the weather", "Good");
+    if (person != null) {
+        document.getElementById("demo").innerHTML =
+        "Its " + person + "! ";
+    
+}

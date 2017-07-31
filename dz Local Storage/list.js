56bf04c72720eba003bb40c8ff1a7e082f0ddf95
Oliinyk Iryna <oleyniktsirina@gmail.com>
2017-07-31 16:45:01 +0300

    Add files via upload

--- dz Local Storage/list.js
+++ dz Local Storage/list.js
@@ -1,42 +1,59 @@
 document.getElementById('submit').addEventListener('click', setItemToLS);
 
 function setItemToLS() {
-    var i = 0;
-    while (localStorage.getItem(i.toString())) {
-        i++;
-    }
-
-    var val = document.getElementsByTagName('form')[0].children[0].value;
-    if ('localstorage' in window || window['localStorage'] !== null) {
-        localStorage.setItem(i.toString(), val);
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount) + 1;
+        } else {
+            localStorage.clickcount = 1;
+        }
+        var i = localStorage.clickcount;
+        var val = document.getElementsByTagName('form')[0].children[0].value;
+        if ('localstorage' in window || window['localStorage'] !== null) {
+            localStorage.setItem(i, val);
+        }
+    } else {
+        console.log("Sorry, your browser does not support web storage...");
     }
 }
 
 
 function getItemToList(key) {
-    var todo = localStorage.getItem(key.toString());
+    var todo = localStorage.getItem(key);
     var el = document.createElement('li');
+    el.setAttribute('id', key);
     el.innerHTML = todo;
     document.getElementById('list').appendChild(el);
     var but = document.createElement('button');
-    var param =  'localStorage.removeItem("'+ key.toString() +'")';
-    but.setAttribute('onclick', param);
     but.setAttribute('class', 'deleteItem');
     but.innerHTML = 'delete';
-    document.getElementById('list').appendChild(but);
+    el.appendChild(but);
 
 }
 
-var j = 0;
-while (localStorage.getItem(j.toString())) {
-    getItemToList(j);
-    j++;
+document.getElementById('list').addEventListener('click', deleteItem);
+
+function deleteItem(event) {
+    if (event.target.className === 'deleteItem') {
+        var key = event.target.parentNode.id;
+        localStorage.removeItem(key);
+        document.getElementById(key).remove();
+
+    }
+}
+
+for (key in localStorage) {
+    if (key === 'clickcount') {
+        continue;
+    }
+    getItemToList(key);
 }
 
 document.getElementById('delete').addEventListener('click', delAll);
 
 function delAll() {
     localStorage.clear();
+    document.getElementById('list').innerHTML = '';
 }
 
 
